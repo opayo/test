80f5e084a6c4d95496ef2e1207c262c5e3ebcc2b
EC2 Default User <ec2-user@ip-172-31-14-70.eu-west-2.compute.internal>
2021-03-19 08:34:27 +0000

    last

--- Requirement1/rds_config.py
+++ Requirement1/rds_config.py
@@ -1,5 +1,5 @@
-DB_HOST = 'opayodbinstance.cqosygi6lkwe.eu-west-2.rds.amazonaws.com'
+DB_HOST = 'opayoddbinstance.cqosygi6lkwe.eu-west-2.rds.amazonaws.com'
 DB_PORT = 3306
 DB_USER = 'admin'
 DB_PASSWORD = 'password'
-DB_NAME = 'opayo'
+DB_NAME = 'Opayo'
